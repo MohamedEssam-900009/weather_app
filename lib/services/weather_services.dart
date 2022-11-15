6ff95b675f70f60f675019c721f78149b7fc390d
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-11-15 10:42:05 +0200

    added weather model class

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/models/weather_model.dart';
 
 class WeatherServices {
   String baseUrl = 'http://api.weatherapi.com/v1';
 
   String apiKey = '509947def5c645b9b1b230652220411&q';
-  void getWeather({required String cityName}) async {
+
+  Future<WeatherModel> getWeather({required String cityName}) async {
     Uri url = Uri.parse(
       '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1',
     );
@@ -16,7 +18,16 @@ class WeatherServices {
 
     Map<String, dynamic> data = jsonDecode(response.body);
 
+    var jsonData = data['forecast']['forecastday'][0]['day'];
 
-    debugPrint(data.toString());
+    WeatherModel weatherModel = WeatherModel(
+      date: data['location']['localtime'],
+      temp: jsonData['avgtemp_c'],
+      maxTemp: jsonData['maxtemp_c'],
+      minTemp: jsonData['mintemp_c'],
+      weatherStateName: jsonData['condition']['text'],
+    );
+
+    return weatherModel;
   }
 }
